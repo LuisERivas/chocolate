15940af505c99ea18bec503517ceccc4ca16d065
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-01-19 13:52:10 -0800

    creeated routes

--- server.js
+++ server.js
@@ -16,5 +16,15 @@ const PORT = process.env.PORT || 5000
 /* Check to make sure API is running */
 app.get('/', (req, res) => res.send('API is Running'))
 
+// Define routes
+
+// make /api/users pertain to the user.js file so that we can use any routes that start with "/"
+// when using /api/users we are really using './routes/api/users'
+app.use('/api/users', require('./routes/api/users'))
+
+// make /api/users pertain to the user.js file so that we can use any routes that start with "/"
+// when using /api/users we are really using './routes/api/users'
+app.use('/api/auth', require('./routes/api/auth'))
+
 /* Listen for Server starting and output the port it started on */
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
