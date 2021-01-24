f838017327f8779ad5c9bc91e8d5c12f935a92bf
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-01-23 20:02:54 -0800

    added navbar component and landing (not production ready)

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// bring in app.css
+import './App.css'
+// bring in fragment from react
+import React, { Fragment } from 'react'
+// import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+// bring in components
+import Navibar from './components/layout/Navibar'
+import Landing from './components/layout/Landing'
+const App = () =>
+  <Fragment>
+    <Navibar />
+    <Landing />
+  </Fragment>
+
+export default App
