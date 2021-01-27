252fabf9e06a2417ff8749af74a822b82a5c2053
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-01-26 17:14:16 -0800

    created alert reducer and added reducer

---
+++ client/src/actions/types.js
@@ -0,0 +1,4 @@
+// all the types of alerts in here
+
+export const SET_ALERT = 'SET_ALERT'
+export const REMOVE_ALERT = 'REMOVE_ALERT'
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,7 @@
+// bring in alert reducer
+import alert from './alert'
+
 import { combineReducers } from 'redux'
-
 export default combineReducers({
-
+  alert
 })
