ccbfd52a92fbb248fdb7a85869b4a854b9e931ee
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-02-14 10:53:06 -0800

    added basic profile model for users

---
+++ models/Profile.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    // used to connect the profile schema to a specific user model by refrencing the id of the user
+    type: mongoose.Schema.Types.ObjectId,
+    // references user model
+    ref: 'user'
+  },
+  // parts of the model
+  experience: {
+    type: String
+  },
+  tradingStyle: {
+    type: String
+  },
+  platform: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
