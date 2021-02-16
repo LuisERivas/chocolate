510415e174272d197948b25ddbb18429768c275d
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-02-15 17:05:48 -0800

    added route to get all profiles and to get profile by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -80,4 +80,43 @@ router.post('/', [ auth, [
 }
 )
 
+// get api/profile
+// get all profiles
+// public access
+router.get('/', async (req, res) => {
+  try {
+    // set profiles to all the profiles we find and populate it with just the user name
+    const profiles = await Profile.find().populate(
+      'user',
+      ['name'])
+    res.json(profiles)
+  } catch (err) {
+    console.error(err.message)
+
+    res.status(500).send('Server Error')
+  }
+})
+
+// get api/profile/user/:user_id
+// get all profiles
+// public access
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    // set profiles to one profile we find and populate it with just the user name
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate(
+      'user',
+      ['name'])
+
+    if (!profile) return res.status(400).json({ msg: 'no profile for this user' })
+
+    res.json(profile)
+  } catch (err) {
+    console.error(err.message)
+    if (err.kind === 'ObjectId') {
+      return res.status(400).json({ msg: 'Not valid profile ID!' })
+    }
+    res.status(500).send('Server Error')
+  }
+})
+
 module.exports = router
