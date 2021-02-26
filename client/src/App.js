3788a74454dad3f18be85514bcfd58e08153bb4a
LuisERivas <lucho.rivas.villacorta@gmail.com>
2021-02-26 12:39:20 -0800

    added stonk input component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Login from './components/auth/Login'
 import Register from './components/auth/Register'
 import Dashboard from './components/dashboard/Dashboard'
 import PrivateRoute from './components/routing/PrivateRoute'
+import PickStonks from './components/profile-forms/PickStonks'
 // Bring in redux
 import { Provider } from 'react-redux'
 import store from './store'
@@ -42,6 +43,8 @@ const App = () => {
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
             <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/pick-stonks' component={PickStonks} />
+
           </Switch>
         </section>
       </Fragment>
